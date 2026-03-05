4839b9d763d31ff22bce3a15338e537380e7e014
Zachary Zrinski <zz@Zacharys-MacBook-Air.local>
2026-03-05 13:14:27 +0800

    seperated into mac/linux/term

--- fish/.config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-alias clear="clear && fastfetch"
-set -x YDOTOOL_SOCKET $HOME/.ydotool_socket
-
---
+++ term/.config/fish/config.fish
@@ -0,0 +1,17 @@
+if test (uname) = "Linux"
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+alias ls='eza -al --color=always --group-directories-first --icons'
+alias clear="clear && fastfetch"
+set -x YDOTOOL_SOCKET $HOME/.ydotool_socket
+
