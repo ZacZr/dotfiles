0835073fc68adca7314595c99d8bf6c7bace19e2
yuh-source <zac.zrinski@gmail.com>
2026-08-08 08:10:32 +0800

    vicinae changes + misc

--- linux/.scripts/autoclick.sh
+++ linux/.scripts/autoclick.sh
@@ -12,4 +12,21 @@ notify-send -u "low" "Autoclick started."
 
 while [ -f "$LOCKFILE" ]; do
     ydotool click --next-delay 2 0xC0
+    # sleep 10
 done
+
+# DELAY=10.0
+# 
+# while [ -f "$LOCKFILE" ]; do
+#     ydotool mousemove --absolute -x 2000 -y 500
+#     sleep 0.1
+#     ydotool mousemove --absolute -x 2010 -y 500
+#     sleep 0.1
+#     ydotool click 0xC0
+# 
+#     ydotool mousemove --absolute -x 2000 -y 600
+#     sleep 0.1
+#     ydotool click 0xC0
+# 
+#     sleep "$DELAY"
+# done
